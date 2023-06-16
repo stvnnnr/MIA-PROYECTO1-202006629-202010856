a55776243ca01563ddbe1b6242ec82d254025b44
Edwin Sandoval <edwinsandoval1000@gmail.com>
2023-06-15 20:34:20 -0600

    fix

--- src/utils/bitacora.py
+++ src/utils/bitacora.py
@@ -11,7 +11,8 @@ def write_log(message):
         date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
         if encrypt_log:
             encrypt = Encrypt()
-            message = encrypt.encrypt_message({date} - {message}, key)
+            message = f'{date} - {message}'
+            message = encrypt.encrypt_message(message, key)
         file.write(f'{message}\n')
 
 def create_folder():
